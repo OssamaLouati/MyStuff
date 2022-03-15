a71fd938f46afce27be91ac712efb2ed1a214cb1
Oussama Louati <92301144+OssamaLouati@users.noreply.github.com>
2022-03-15 23:17:56 +0100

    update

---
+++ test/1.js
@@ -0,0 +1,11 @@
+function multiply(){
+    n1 = document.getElementById("firstnumber").value;
+    n2 = document.getElementById("secondnumber").value;
+    document.getElementById("res").innerHTML = n1 * n2;
+}
+function divide(){
+    num1 = document.getElementById("firstnumber").value;
+    num2 = document.getElementById("secondnumber").value;
+    document.getElementById("res").innerHTML = num1 / num2;
+
+}
