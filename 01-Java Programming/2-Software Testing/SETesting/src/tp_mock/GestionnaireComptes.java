bea91589e49f732e651b39c08bc007d4ae147fe3
Oussama Louati <92301144+OssamaLouati@users.noreply.github.com>
2022-03-20 11:45:44 +0100

    commit

--- 01-Java Programming/2-Software Testing/SETesting/src/tp_mock/GestionnaireComptes.java
+++
@@ -1,8 +0,0 @@
-package tp_mock;
-
-public interface GestionnaireComptes {
-	public Compte rechercherParNomUtilisateur(String nomUtilisateur) 
-			throws CompteInexistantException;
-	public Compte creerCompte(String nomUtilisateur, String motPasse);
-	public boolean supprimerCompte(Compte compte);
-}
